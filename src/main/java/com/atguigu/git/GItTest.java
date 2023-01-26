d5d94c7d144cb8354927868c3d485e6d301066b2
lhc <1406415492@qq.com>
2023-01-26 16:23:05 +0800

    hot-fix commit 2

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -8,6 +8,6 @@ public class GItTest {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
-        System.out.println("hello git4!");
+        System.out.println("hot-fix test!");
     }
 }
