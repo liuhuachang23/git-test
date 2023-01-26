80ebcb23e6cedea8dafd8b21ee64d7b4ed7e1444
lhc <1406415492@qq.com>
2023-01-26 15:55:40 +0800

    third commit

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -7,5 +7,6 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
