f1309b526ec79694cb439a942e1c147a13e1b152
lhc <1406415492@qq.com>
2023-01-26 16:52:02 +0800

    push commit

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -11,5 +11,6 @@ public class GItTest {
         System.out.println("hello git4!");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
