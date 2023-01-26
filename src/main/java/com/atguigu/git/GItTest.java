9796ec951792f3aba6e330b8cdd9e751ef7a5265
lhc <1406415492@qq.com>
2023-01-26 16:24:34 +0800

    master commit 2

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -9,5 +9,6 @@ public class GItTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("master test!");
     }
 }
