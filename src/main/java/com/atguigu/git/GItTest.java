2e485c807850050c2e81a59da6bba786d011f889
lhc <1406415492@qq.com>
2023-01-26 15:54:45 +0800

    second commit

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -6,5 +6,6 @@ package com.atguigu.git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
